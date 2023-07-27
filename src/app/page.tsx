edd330250fbd7713f1530301cf0004b2a1d7d5fc
Elias Gabriel <eliasgabrielcf@gmail.com>
2023-07-27 11:10:10 -0300

    Fundamentos do Tailwind - Responsividade & Breakpoints

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
     <div className="p-8 bg-slate-900 text-slate-100 h-screen">
-      <h1 className="font-bold text-5xl flex items-center gap-3 before:w-0.5 before:h-8 before:bg-sky-500 before:flex">
+      <h1 className="font-bold text-3xl sm:text-5xl lg:text-6xl flex items-center gap-3 before:w-0.5 before:h-8 before:bg-sky-500 before:flex">
         Hello Tailwind
       </h1>
       <h2>Hello Tailwind</h2>
