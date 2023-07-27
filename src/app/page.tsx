b32103849cf75f642365c69dd7528494fa29c4a6
Elias Gabriel <eliasgabrielcf@gmail.com>
2023-07-27 11:18:15 -0300

    Fundamentos do Tailwind - Dark Mode no Tailwind

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,15 @@
 export default function Home() {
   return (
-    <div className="p-8 bg-slate-900 text-slate-100 h-screen">
-      <h1 className="font-bold text-3xl sm:text-5xl lg:text-6xl flex items-center gap-3 before:w-0.5 before:h-8 before:bg-sky-500 before:flex">
-        Hello Tailwind
-      </h1>
-      <h2>Hello Tailwind</h2>
-      <p>Hello Tailwind</p>
-      <button disabled className="bg-sky-500 px-4 py-2 rounded-md font-medium mt-4 enabled:hover:bg-sky-600 disabled:opacity-60 disabled:cursor-not-allowed">
-        Sign In
-      </button>
+    <div className="p-8 bg-slate-50 text-slate-900 dark:bg-slate-900 dark:text-slate-100 h-screen flex flex-col items-center text-center">
+      <div className="max-w-2xl">
+        <h1 className="font-bold text-3xl sm:text-4xl lg:text-5xl">
+          Rapidly build modern websites without ever leaving your HTML.
+        </h1>
+        <p className="mt-4 dark:text-slate-400">A utility-first CSS framework packed with classes like flex, pt-4, text-center and rotate-90 that can be composed to build any design, directly in your markup.</p>
+        <button className="bg-sky-500 dark:bg-sky-400 dark:text-sky-950 text-white px-4 py-2 rounded-md font-medium mt-4 enabled:hover:bg-sky-600 disabled:opacity-60 disabled:cursor-not-allowed">
+          Sign In
+        </button>
+      </div>
     </div>
   )
 }
