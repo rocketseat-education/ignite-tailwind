f1f9def9de5dfcd98128e7889342ea7057404f67
Elias Gabriel <eliasgabrielcf@gmail.com>
2023-07-27 11:06:18 -0300

    Fundamentos do Tailwind - Seletores e estados

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,14 @@
 export default function Home() {
   return (
     <div className="p-8 bg-slate-900 text-slate-100 h-screen">
-      <h1 className="font-bold text-5xl">Hello Tailwind</h1>
+      <h1 className="font-bold text-5xl flex items-center gap-3 before:w-0.5 before:h-8 before:bg-sky-500 before:flex">
+        Hello Tailwind
+      </h1>
       <h2>Hello Tailwind</h2>
       <p>Hello Tailwind</p>
+      <button disabled className="bg-sky-500 px-4 py-2 rounded-md font-medium mt-4 enabled:hover:bg-sky-600 disabled:opacity-60 disabled:cursor-not-allowed">
+        Sign In
+      </button>
     </div>
   )
 }
