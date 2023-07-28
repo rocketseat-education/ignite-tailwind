b48a4268e3aca79e7c567434cc0f76e5e9ab4cec
Elias Gabriel <eliasgabrielcf@gmail.com>
2023-07-28 11:42:19 -0300

    Formulário - Estrutura do formulário

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -20,7 +20,7 @@ export default function RootLayout({
       <body className={inter.className}>
         <div className="grid min-h-screen grid-cols-app">
           <Sidebar />
-          <main className="px-4 pb-12 pt-8">{children}</main>
+          <main className="px-8 pb-12 pt-8">{children}</main>
         </div>
       </body>
     </html>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,6 +6,36 @@ export default function Home() {
       <h1 className="to-zinc-900 text-3xl font-medium">Settings</h1>
 
       <SettingsTabs />
+
+      <div className="mt-6 flex flex-col">
+        <div className="flex items-center justify-between border-b border-zinc-200 pb-5">
+          <div className="space-y-1">
+            <h2 className="text-lg font-medium text-zinc-900">Personal info</h2>
+            <span className="text-sm text-zinc-500">
+              Update your photo and personal details here.
+            </span>
+          </div>
+          <div className="flex items-center gap-2">
+            <button
+              type="button"
+              className="rounded-lg border border-zinc-300 px-4 py-2 text-sm font-semibold text-zinc-700 shadow-sm hover:bg-zinc-50"
+            >
+              Cancel
+            </button>
+            <button
+              type="submit"
+              form="settings"
+              className="rounded-lg bg-violet-600 px-4 py-2 text-sm font-semibold text-white shadow-sm hover:bg-violet-700"
+            >
+              Save
+            </button>
+          </div>
+        </div>
+
+        <form id="settings" className="mt-6 flex w-full flex-col">
+          form
+        </form>
+      </div>
     </>
   )
 }
