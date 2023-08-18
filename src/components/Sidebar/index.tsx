a672ce54a4812612c68ae58f117def824622aeba
Elias Gabriel <eliasgabrielcf@gmail.com>
2023-08-18 11:11:35 -0300

    Responsividade e dark mode - Responsividade da Sidebar

--- src/components/Sidebar/index.tsx
+++ src/components/Sidebar/index.tsx
@@ -17,7 +17,7 @@ import * as Input from '@/components/Input'
 
 export function Sidebar() {
   return (
-    <aside className="flex flex-col gap-6 border-r border-zinc-200 px-5 py-8">
+    <aside className="fixed bottom-0 left-0 right-0 top-0 z-20 flex flex-col gap-6 border-b border-zinc-200 bg-white p-4 lg:relative lg:right-auto lg:w-80 lg:border-r lg:px-5 lg:py-8">
       <Logo />
 
       <Input.Root>
